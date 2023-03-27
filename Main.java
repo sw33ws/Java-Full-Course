8b855a4183837646d608b71ecde9d056d1c67ae7
sw33ws <gillisl22@outlook.com>
2023-03-27 08:05:00 -0400

    more java learned

--- Main.java
+++ Main.java
@@ -625,6 +625,7 @@
 
 
 
+// OOP
 // public class Main {
 
 //     public static void main(String[] args) {
@@ -643,6 +644,34 @@
 
 //         // myCar1.drive();
 //         // myCar1.brake();
+//     }
+// }
+
+
+
+// // Constructors
+// public class Main {
+
+//     public static void main(String[] args) {
+        
+//         Human human1 = new Human("Brick",65,140);
+//         Human human2 = new Human("mordecai",59,75);
+
+//         System.out.println(human1.name);
+//         System.out.println(human2.name);
+
+//         human1.eat();
+//         human2.drink();
+//     }
+// }
+
+
+
+// public class Main {
+
+//     public static void main(String[] args) {
+        
+//         DiceRoller diceRoller = new DiceRoller();
 //     }
 // }
 
