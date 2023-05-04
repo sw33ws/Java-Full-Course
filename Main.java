24d5fade0962bae57cca3090256805064dae4256
sw33ws <gillisl22@outlook.com>
2023-05-04 08:03:25 -0400

    done

--- Main.java
+++ Main.java
@@ -1264,11 +1264,57 @@
 
 
 
+// public class Main {
+
+//      public static void main(String[] args) {
+            
+//         new MyFrame();
+//     }
+// }
+
+
+
+
+// import java.awt.BorderLayout;
+// import java.awt.Color;
+// import java.awt.LayoutManager;
+
+// import javax.swing.JFrame;
+// import javax.swing.JPanel;
+
+// public class Main {
+
+//      public static void main(String[] args) {
+            
+//         JFrame frame = new JFrame();
+//         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//         frame.setSize(500, 500);
+//         frame.setLayout(new BorderLayout(10, 10));
+//         frame.setVisible(true);
+
+//         JPanel panel1 = new JPanel();
+//         JPanel panel2 = new JPanel();
+//         JPanel panel3 = new JPanel();
+//         JPanel panel4 = new JPanel();
+//         JPanel panel5 = new JPanel();
+
+//         panel1.setBackground(Color.red);
+//         panel2.setBackground(Color.green);
+//         panel3.setBackground(Color.yellow);
+//         panel4.setBackground(Color.magenta);
+//         panel5.setBackground(Color.blue);
+
+//         frame.add(panel1, BorderLayout.NORTH);
+//     }
+// }
+
+
+
 public class Main {
 
      public static void main(String[] args) {
             
-        new MyFrame();
+        
     }
 }
 
--- MyFrame.java
+++ MyFrame.java
@@ -22,62 +22,62 @@
 
 
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+// import java.awt.Color;
+// import java.awt.Font;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+// import javax.swing.BorderFactory;
+// import javax.swing.ImageIcon;
+// import javax.swing.JButton;
+// import javax.swing.JFrame;
+// import javax.swing.JLabel;
 
-public class MyFrame extends JFrame implements ActionListener{
+// public class MyFrame extends JFrame implements ActionListener{
 
-    JButton button;
-    JLabel label;
+//     JButton button;
+//     JLabel label;
 
-    MyFrame() {
+//     MyFrame() {
 
-        ImageIcon icon = new ImageIcon("logo.png");
-        ImageIcon icon2 = new ImageIcon("logo.png");
+//         ImageIcon icon = new ImageIcon("logo.png");
+//         ImageIcon icon2 = new ImageIcon("logo.png");
 
-        label = new JLabel();
-        label.setIcon(icon2);
-        label.setBounds(150, 250, 150, 150);
-        label.setVisible(false);
+//         label = new JLabel();
+//         label.setIcon(icon2);
+//         label.setBounds(150, 250, 150, 150);
+//         label.setVisible(false);
 
-        button = new JButton();
-        button.setBounds(75, 75, 350, 300);
-        button.addActionListener(this);
-        // button.addActionListener(e -> System.out.println("hello"));
-        button.setText("I'm a button!");
-        button.setFocusable(false);
-        button.setIcon(icon);
-        button.setHorizontalTextPosition(JButton.CENTER);
-        button.setVerticalTextPosition(JButton.BOTTOM);
-        button.setFont(new Font("Comic Sans",Font.BOLD,25));
-        button.setIconTextGap(-2);
-        button.setForeground(Color.cyan);
-        button.setBackground(Color.lightGray);
-        button.setBorder(BorderFactory.createEtchedBorder());
-        // button.setEnabled(false); // turns off button
+//         button = new JButton();
+//         button.setBounds(75, 75, 350, 300);
+//         button.addActionListener(this);
+//         // button.addActionListener(e -> System.out.println("hello"));
+//         button.setText("I'm a button!");
+//         button.setFocusable(false);
+//         button.setIcon(icon);
+//         button.setHorizontalTextPosition(JButton.CENTER);
+//         button.setVerticalTextPosition(JButton.BOTTOM);
+//         button.setFont(new Font("Comic Sans",Font.BOLD,25));
+//         button.setIconTextGap(-2);
+//         button.setForeground(Color.cyan);
+//         button.setBackground(Color.lightGray);
+//         button.setBorder(BorderFactory.createEtchedBorder());
+//         // button.setEnabled(false); // turns off button
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(500, 500);
-        this.setVisible(true);
-        this.add(button);
-        this.add(label);
-    }
+//         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//         this.setLayout(null);
+//         this.setSize(500, 500);
+//         this.setVisible(true);
+//         this.add(button);
+//         this.add(label);
+//     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==button) {
-            System.out.println("hello");
-            button.setEnabled(false);
-            label.setVisible(true);
-        }
-    }
-}
+//     @Override
+//     public void actionPerformed(ActionEvent e) {
+//         if(e.getSource()==button) {
+//             System.out.println("hello");
+//             button.setEnabled(false);
+//             label.setVisible(true);
+//         }
+//     }
+// }
