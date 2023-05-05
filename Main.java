dee57392268f5e29be3d94632fdca2eea15065e0
sw33ws <gillisl22@outlook.com>
2023-05-05 10:12:15 -0400

    layered Pane

--- Main.java
+++ Main.java
@@ -1310,6 +1310,8 @@
 
 
 
+// FlowLayout
+
 // import java.awt.Color;
 // import java.awt.Dimension;
 // import java.awt.FlowLayout;
@@ -1342,6 +1344,83 @@
 //         panel.add(new JButton("9"));
 
 //         frame.add(panel);
+//         frame.setVisible(true);
+//     }
+// }
+
+
+
+// Grid Layout
+
+// import java.awt.GridLayout;
+// import javax.swing.JButton;
+// import javax.swing.JFrame;
+
+// public class Main {
+
+//      public static void main(String[] args) {
+            
+//         JFrame frame = new JFrame();
+//         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//         frame.setSize(500, 500);
+//         frame.setLayout(new GridLayout(3, 3, 10, 10));
+
+//         frame.add(new JButton("1"));
+//         frame.add(new JButton("2"));
+//         frame.add(new JButton("3"));
+//         frame.add(new JButton("4"));
+//         frame.add(new JButton("5"));
+//         frame.add(new JButton("6"));
+//         frame.add(new JButton("7"));
+//         frame.add(new JButton("8"));
+//         frame.add(new JButton("9"));
+
+//         frame.setVisible(true);
+//     }
+// }
+
+
+
+// Layered Pane
+
+// import javax.swing.*;
+// import java.awt.*;
+
+// public class Main {
+
+//      public static void main(String[] args) {
+
+//         JLabel label1 = new JLabel();
+//         label1.setOpaque(true);
+//         label1.setBackground(Color.RED);
+//         label1.setBounds(50, 50, 200, 200);
+
+//         JLabel label2 = new JLabel();
+//         label2.setOpaque(true);
+//         label2.setBackground(Color.GREEN);
+//         label2.setBounds(100, 100, 200, 200);
+
+//         JLabel label3 = new JLabel();
+//         label3.setOpaque(true);
+//         label3.setBackground(Color.BLUE);
+//         label3.setBounds(150, 150, 200, 200);
+            
+//         JLayeredPane layeredPane = new JLayeredPane();
+//         layeredPane.setBounds(0, 0, 500, 500);
+
+//         // layeredPane.add(label1, JLayeredPane.DEFAULT_LAYER);
+//         // layeredPane.add(label2, JLayeredPane.DEFAULT_LAYER);
+//         // layeredPane.add(label3, JLayeredPane.DRAG_LAYER);
+
+//         layeredPane.add(label1, Integer.valueOf(0));
+//         layeredPane.add(label2, Integer.valueOf(2));
+//         layeredPane.add(label3, Integer.valueOf(1));
+
+//         JFrame frame = new JFrame("JLayeredPane");
+//         frame.add(layeredPane);
+//         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//         frame.setSize(new Dimension(500, 500));
+//         frame.setLayout(null);
 //         frame.setVisible(true);
 //     }
 // }
