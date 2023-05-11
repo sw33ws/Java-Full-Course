e59d38ffa865402580f702d22dc510fb812c4237
sw33ws <gillisl22@outlook.com>
2023-05-11 10:29:42 -0400

    Generics

--- Main.java
+++ Main.java
@@ -1653,6 +1653,61 @@
 
 
 
+// // Generics
+
+// public class Main {
+
+//      public static void main(String[] args) {
+            
+//         // Integer[] intArray = {1, 2, 3, 4, 5};
+//         // Double[] doubleArray = {5.5, 4.4, 3.3, 2.2, 1.1};
+//         // Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
+//         // String[] stringArray = {"B", "Y", "E"};
+
+//         // displayArray(intArray);
+//         // displayArray(doubleArray);
+//         // displayArray(charArray);
+//         // displayArray(stringArray);
+
+//         // System.out.println(getFirst(intArray));
+//         // System.out.println(getFirst(doubleArray));
+//         // System.out.println(getFirst(charArray));
+//         // System.out.println(getFirst(stringArray));
+
+//         Player player = new Player();
+//         Enemy enemy = new Enemy();
+//         Item item = new Item();
+//         Tree tree = new Tree();
+
+//         draw(player);
+//         draw(enemy);
+//         draw(item);
+//         draw(tree);
+//     }
+
+//     // public static <T> void displayArray(T[] array) {
+
+//     //     // T isn't needed, its just good practice
+//     //     for(T x : array) {
+//     //         System.out.println(x+" ");
+//     //     }
+//     //     System.out.println();
+//     // }
+
+//     // public static <T> T getFirst(T[] array) {
+
+//     //     return array[0];
+//     // }
+
+//     public static <T> void draw(T x) {
+//         x.draw();
+//     }
+// }
+
+
+
+// Generic Classes
+
 public class Main {
 
      public static void main(String[] args) {
