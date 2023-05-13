f78de189b67aa66507d4ef9fd477db06952e3bc0
sw33ws <gillisl22@outlook.com>
2023-05-12 21:54:04 -0400

    timer task

--- Main.java
+++ Main.java
@@ -1751,6 +1751,57 @@
 
 
 
+// // Timer
+
+// import java.util.Calendar;
+// import java.util.Timer;
+// import java.util.TimerTask;
+
+// public class Main {
+
+//      public static void main(String[] args) {
+            
+//         Timer timer = new Timer();
+
+//         TimerTask task = new TimerTask() {
+
+//             int counter = 10;
+
+//             @Override
+//             public void run() {
+//                 // System.out.println("Task is complete!");
+
+//                 if(counter>0) {
+//                     System.out.println(counter+" seconds");
+//                     counter--;
+//                 }
+//                 else {
+//                     System.out.println("Happy New Year");
+//                     timer.cancel();
+//                 }
+//             }
+//         };
+
+//         Calendar date = Calendar.getInstance();
+//         date.set(Calendar.YEAR,2023);
+//         date.set(Calendar.MONTH,Calendar.MAY);
+//         date.set(Calendar.DAY_OF_MONTH,12);
+//         date.set(Calendar.HOUR_OF_DAY,21);
+//         date.set(Calendar.MINUTE,53);
+//         date.set(Calendar.SECOND,0);
+//         date.set(Calendar.MILLISECOND,0);
+
+//         // timer.schedule(task, date.getTime());
+
+//         // timer.schedule(task, 3000);
+
+//         // timer.scheduleAtFixedRate(task, 0, 1000);
+//         timer.scheduleAtFixedRate(task, date.getTime(), 1000);
+//     }
+// }
+
+
+
 public class Main {
 
      public static void main(String[] args) {
